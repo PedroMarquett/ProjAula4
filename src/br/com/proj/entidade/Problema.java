136588262c65cb7f7331aa084e367769ba6463a2
Pedro <brasil.ph@hotmail.com>
2018-08-28 20:24:40 -0300

    Atualizacao

---
+++ src/br/com/proj/entidade/Problema.java
@@ -0,0 +1,26 @@
+package br.com.proj.entidade;
+
+public class Problema {
+	private int id;
+	private String descricao;
+	private String nome;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
---
+++ src/br/com/proj/teste/Teste.java
@@ -0,0 +1,7 @@
+package br.com.proj.teste;
+
+public class Teste {
+	public static main(args) {
+		
+	}
+}
